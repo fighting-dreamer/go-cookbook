4d17f5eefca3eff90cd324e6c9d7df4408a596ac
nipun.jindal <nipun.jindal@go-jek.com>
2020-02-22 02:33:04 +0530

    using waitgroups to run go-routines and wait for them to complete

--- understanding-concurrency/basics/concurrent_count.go
+++ understanding-concurrency/basics/concurrent_count.go
@@ -2,12 +2,23 @@ package basics
 
 import (
 	"fmt"
+	"time"
+	"sync"
 )
 
 // infinetily printin index along with same input
 func count(input string) {
 	for i := 0; true; i++ {
 		fmt.Println(i, input)
+	}
+}
+
+// infinetily printin index along with same input
+func count2(input string) {
+	halfSecond := time.Millisecond*500
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i, input)
+		time.Sleep(halfSecond)
 	}
 }
 
@@ -28,8 +39,38 @@ func func3() {
 	go count("fish")
 }
 
+
+// this is blocking, will go-routine completes, here it will run infinitely
+func func4() {
+	var wg sync.WaitGroup
+	wg.Add(1) // imlies , I am waiting for '1' go-routine to complete
+
+	go func() {
+		count("sheep")
+		wg.Done()
+	}() // anonymous function, coz, the wait-group related changes are not responsibility of go-routine
+	wg.Wait()
+}
+
+// this is blocking, will go-routine completes, here it will run infinitely
+func func5() {
+	var wg sync.WaitGroup
+	wg.Add(1) // imlies , I am waiting for '1' go-routine to complete
+
+	go func() {
+		count2("sheep")
+		wg.Done()
+	}() // anonymous function, coz, the wait-group related changes are not responsibility of go-routine
+	wg.Wait()
+}
+
+
 func Start() {
 	//func1()
 	//func2()
 	//func3()
+
+	//With Waitgroup
+	//func4()
+	func5()
 }
