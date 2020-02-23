f0dd2a422347c61a75497c3875a55c869e06dcb4
nipun.jindal <nipun.jindal@go-jek.com>
2020-02-23 16:37:52 +0530

    hello world , printing command line arguments

--- system-go/chapter01/cmd/hw_main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (ch1 "nipun.io/go-cookbook/system-go/chapter01")
-
-func main() {
-	ch1.Start()
-}
-
-
--- system-go/main.go
+++ system-go/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	ch1.Start()
+	ch1.Start2()
 }
